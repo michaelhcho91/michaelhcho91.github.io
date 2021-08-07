fd631ce065cfa286a2134080ceadee72fa52e379
Michael Cho <michaelhcho91@gmail.com>
2021-08-06 21:22:22 -0400

    Add extracurricular section

--- src/js/Extra.jsx
+++ src/js/Extra.jsx
@@ -15,15 +15,26 @@ const propTypes = {
 };
 
 const Extra = ({ item }) => {
-  const { name, school, logo } = item;
+  const { name, school, logo, duration, position, keyPoints } = item;
 
   return (
     <li className="extra item">
       <Logo logo={logo} alt={name} />
 
       <div className="details">
+        <IconLabel icon="user" label={position} />
+        <IconLabel icon="users" label={name} />
         <IconLabel icon="university" label={school} />
+        <IconLabel icon="calendar" label={duration} />
       </div>
+
+      <ul className="key-points">
+        {keyPoints.map((point, idx) => {
+          return (
+            <li key={idx + point[0]}>{point}</li>
+          );
+        })}
+      </ul>
     </li>
   );
 };
